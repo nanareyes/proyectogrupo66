c3c630e5237ef925a46f1df981740f1557b5d77c
nanareyes <dianac.reyesmantilla@gmail.com>
2021-07-07 09:07:35 -0500

    Se agregan el set y el get al código

--- Tiendas/src/Producto.java
+++ Tiendas/src/Producto.java
@@ -7,4 +7,21 @@ public class Producto {
         System.out.println(nombre + ": " + precio);
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public float getPrecio() {
+        return this.precio;
+    }
 }

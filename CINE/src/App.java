6517ced36dbd5c718bd0d61b90ac015cea07b542
nanareyes <dianac.reyesmantilla@gmail.com>
2021-07-08 17:05:39 -0500

    Se actualiza el nombre del cine

--- CINE/src/App.java
+++ CINE/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bienvenido al cine");
+        System.out.println("Bienvenido al cine XX");
     }
 }
